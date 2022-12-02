e03f61ffc76b123893cd00f90622625cec6e34a9
«Alexandr» <nazgul.nn@gmail.com>
2022-12-02 17:30:38 +0300

    привет маша

---
+++ Ex005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Првиет! Как тебя зовут?");
+string user = Console.ReadLine();
+if(user== "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+   Console.Write("А, это снова ");
+   Console.WriteLine(user);
+}
