3df0959b24ef57c8675450fb48ab28a5da11a782
«Alexandr» <nazgul.nn@gmail.com>
2022-12-02 17:13:30 +0300

    Random

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); //случайное чисто от 1 до 9
+int numberB = new Random().Next(1,10);
+Console.Write("Число 1:" );
+Console.WriteLine(numberA);
+Console.Write("Число 2:" );
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
