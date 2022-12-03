a185392cc28b5e318bb83d527c54b9b6f384f55b
«Alexandr» <nazgul.nn@gmail.com>
2022-12-03 17:46:24 +0300

    sqr = or no number

---
+++ Seminar/Ex002_sqr_of_number/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.Clear();
+Console.WriteLine("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}, {b}");
+
+//if(Math.Pow(b, 2) == a){ Через функцию
+if(a == b*b){
+    Console.WriteLine("Yes");
+}else{
+    Console.WriteLine("No");
+}    
