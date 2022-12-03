4a318cb13849159a54f4faeb809130d0d5badc9f
«Alexandr» <nazgul.nn@gmail.com>
2022-12-03 16:53:49 +0300

    Квадрат

---
+++ Seminar/Ex001_sqr/Program.cs
@@ -0,0 +1,13 @@
+﻿// ввести число с консоли, возвести число в квадрат (number*number)
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr=number*number;
+Console.WriteLine($"Квадрат числа {number} = {sqr}");
+
+//Решение с использ. библиотеки.
+int sqr1=Convert.ToInt32(Math.Pow(number, 2)); //Посмотреть Pow 
+Console.WriteLine($"Квадрат числа {number} = {sqr1}");
+double number2 = 3.9;
+double sqr2=(Math.Pow(number2, 2));
+Console.WriteLine($"Квадрат числа {number2} = {Math.Round(sqr2, 2)}"); // time 1:05:17
