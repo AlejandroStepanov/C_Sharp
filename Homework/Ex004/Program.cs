174dd266fb6242d6237633a85bccc6c25260d884
«Alexandr» <nazgul.nn@gmail.com>
2022-12-04 23:28:48 +0300

    HW Ex004

---
+++ Homework/Ex004/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+int max = a; //если введено несколько одинаковых чисел
+Console.WriteLine("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите c: ");
+int c = int.Parse(Console.ReadLine());
+int count = 1; //счётчик одинаковых значений
+
+if(b>a && b>c)
+{max = b;}
+if(b==a)
+{count+=1;}
+if(c>a && c>b)
+{max = c;}
+if(c==a||c==b)
+{count+=1;}
+    Console.WriteLine($"Max = {max}");
+    Console.WriteLine($"Count of Max = {count}");
+
+    
+
+    
