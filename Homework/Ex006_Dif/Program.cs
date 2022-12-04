8dc0d9a02ed204e9184bf39860721e4bb8e55f93
«Alexandr» <nazgul.nn@gmail.com>
2022-12-04 23:55:15 +0300

    HW Ex006

---
+++ Homework/Ex006_Dif/Program.cs
@@ -0,0 +1,11 @@
+﻿//определение четности числа
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a%2 == 0)
+{
+    Console.WriteLine("Это чётное число.");  
+}else{
+    Console.WriteLine("Это нечётное число."); 
+}
