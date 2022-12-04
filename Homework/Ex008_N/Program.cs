7791d58d3dd143118385341c5399cf62e02b4754
«Alexandr» <nazgul.nn@gmail.com>
2022-12-05 00:14:35 +0300

    HW Ex008

---
+++ Homework/Ex008_N/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывести четные от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Вы ввели чило: {N}");
+int a = 1;
+Console.Write($"Чётные числа от 1 до {N}: ");
+
+while(a <= N)
+{
+   if(a % 2 == 0)
+   {
+    Console.Write($"{a} ");
+   }
+    a++;
+}
