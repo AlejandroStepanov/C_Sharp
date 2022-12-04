ce92117eca7668e899ef1c50ebe8f89e543e8ccd
«Alexandr» <nazgul.nn@gmail.com>
2022-12-04 22:37:38 +0300

    HW min-max

---
+++ Homework/Ex002 min max/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine($"Max = {a}, Min = {b}");
+}else{
+    Console.WriteLine($"Max = {b}, Min = {a}");
+}    
