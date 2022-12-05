8eca79ef3532bd9f6f3258f54447df51ce8ed206
«Alexandr» <nazgul.nn@gmail.com>
2022-12-06 00:45:22 +0300

    Ex017

---
+++ Seminar/Ex017_Координаты/Program.cs
@@ -0,0 +1,28 @@
+﻿// x;y -> zone?
+//ввести трёхзначное число -> вторую цифру
+Console.Clear();
+Console.WriteLine("Введите число X");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число Y");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Zone = 1");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Zone = 2");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("Zone = 3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("Zone = 4");
+}
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("No than zone");
+}
