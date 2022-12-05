779fd7d5ce687dee39717cc8c1eb7ec4accc76d3
«Alexandr» <nazgul.nn@gmail.com>
2022-12-06 02:54:20 +0300

    Ex022

--- Seminar/Ex022/Program.cs
+++ Seminar/Ex022/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+
+// while(i <= n)
+// {
+//     Console.Write($"{Math.Pow(i, 2)} ");
+//     i+=1;
+// }
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($" {i * i} ");
+}
