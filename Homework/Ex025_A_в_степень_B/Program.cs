c933249335fd90ba71e4353f7a69e8da74cec759
«Alexandr» <nazgul.nn@gmail.com>
2022-12-08 02:20:12 +0300

    a^b

---
+++ Homework/Ex025_A_в_степень_B/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+double ab = Math.Pow(a, b);
+Console.WriteLine($"{a}^{b} = {ab}");
+
