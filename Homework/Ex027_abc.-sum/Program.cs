5df47cce4fc95378f2627f9aa1885fc9127a431c
«Alexandr» <nazgul.nn@gmail.com>
2022-12-08 03:05:33 +0300

    HW Ex027

---
+++ Homework/Ex027_abc.-sum/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("Enter the number: ");
+int num = int.Parse(Console.ReadLine());
+int a = num;
+int length =  num.ToString().Length;
+int[] array = new int[length];
+
+for(int i = length -1; i>=0; i--)
+{
+    array[i] = num%10;
+    num = num/10;
+}
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position<count)
+//     {
+//         Console.Write($"{col[position]} ");
+//         position++;
+//     }
+// }
+// PrintArray(array);
+
+
+int Sum(int[] array)
+{
+    int result = 0;
+    for(int j = 0; j<array.Length; j++)
+    {
+      result = result + array[j];  
+    }
+    return result;
+}
+int sum = Sum(array);
+
+Console.WriteLine($"Сумма цифр числа '{a}' = {sum}");
