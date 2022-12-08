6a7dc46d6b1deb4f8362faf7b7019f06a5bc87ec
«Alexandr» <nazgul.nn@gmail.com>
2022-12-09 01:00:41 +0300

    HW Ex034

---
+++ Homework/Ex034_трехзнчное_чет_нечет/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = MakeArrRandon(20);
+Console.WriteLine($"Сгенерированный массив: [{String.Join(",", array)}]");
+Count(array);
+
+int[] MakeArrRandon(int size)
+{
+    int[] random = new int[size];
+    for (int i = 0; i < random.Length; i++)
+    {
+        random[i] = new Random().Next(99, 1000);
+    }
+    return random;
+}
+
+void Count(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+
+    }
+    Console.WriteLine($"Количество чётных чисел в массиве = {count}");
+}
