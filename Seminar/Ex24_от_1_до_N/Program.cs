c0e0e4b2820ce7ea9191015a9ae712a1ab4a870e
«Alexandr» <nazgul.nn@gmail.com>
2022-12-08 16:01:19 +0300

    Ex24

---
+++ Seminar/Ex24_от_1_до_N/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сумма от 1 до N
+Console.Clear();
+Console.WriteLine("Enter the number: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {N} = {Sum(N)}");
+int Sum(int N)
+{
+    int result = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        result +=i;
+    }
+    return result;
+}
