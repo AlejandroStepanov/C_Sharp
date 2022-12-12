7fe75b086ec9b5a34502a82222dfcc69e629c1d3
«Alexandr» <nazgul.nn@gmail.com>
2022-12-13 01:01:48 +0300

    44

---
+++ Seminar/Ex039_returnArray/Program.cs
@@ -0,0 +1,34 @@
+﻿//
+
+Console.Clear();
+
+
+
+int[] GetArray(int size, int minRandom, int maxRandom)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minRandom, maxRandom + 1);
+    }
+    return result;
+
+}
+
+int[] ReversArray(int[] collection)
+{
+    int[] result = new int[collection.Length];
+    for (int i = 0; i < collection.Length; i++)
+    {
+        result [i] = collection[collection.Length - 1 -i];
+    }
+
+
+    // Console.WriteLine($"[{String.Join(",", result)}]");
+    return result;
+}
+
+int[] newArray = GetArray(5, 1, 15);
+Console.WriteLine($"[{String.Join(",", newArray)}]");
+int[] reverseArray = ReversArray(newArray);
+Console.WriteLine($"[{String.Join(",", reverseArray)}]");
---
+++ Seminar/Ex040/Program.cs
@@ -0,0 +1,22 @@
+﻿// **Задача 40:**Напишите программу, 
+// которая принимает на вход три числа и проверяет, может ли 
+// существовать треугольник с сторонами такой длины.
+
+
+// ❗ **Теорема о неравенстве треугольника:
+// **каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Clear();
+Console.Write("Введите стороны треугольника через пробел: ");
+
+string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int a = int.Parse(st[0]), b = int.Parse(st[1]), c = int.Parse(st[2]);
+
+bool Triangle(int a, int b, int c)
+{
+    return (a < (b + c) && b < (a + c) && c < (a + b));
+}
+
+if (Triangle(a, b, c))
+Console.WriteLine("Yes");
+else Console.WriteLine("No");
