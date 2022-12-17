780cf0b255f3c97d72f390288deb7c6852e8a733
«Alexandr» <nazgul.nn@gmail.com>
2022-12-17 14:45:01 +0300

    057

--- Seminar/Ex057_Словарь/Program.cs
+++ Seminar/Ex057_Словарь/Program.cs
@@ -1,36 +1,96 @@
-﻿// Составить частотный словарь элементов двумерного массива. 
-// Частотный словарь содержит информацию о том,
-//  сколько раз встречается элемент входных данных
-
+﻿//Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
 using System;
 using static System.Console;
 
 Clear();
-int[,] matrix = new int[5, 5];
 
-void FillMatrix(int[,] matr, int minValue, int maxValue)
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+int[] rowAr=GetRowArray(array);
+SortArray(rowAr);
+WriteLine(String.Join(" ",rowAr));
+PrintData(rowAr);
+
+int[,] GetArray(int m, int n, int min, int max)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < n; j++)
         {
-            matr[i, j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+int[] GetRowArray(int[,] inArray)
+{
+    int[] result = new int[inArray.GetLength(0)*inArray.GetLength(1)];
+    int index=0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            result[i * inArray.GetLength(1) + j] = inArray[i,j];
+            index++;
+        }
+    }
+    return result;
+}
+
+void SortArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        for (int j = i+1; j < inArray.Length; j++)
+        {
+            if(inArray[i]>inArray[j])
+            {
+                int k=inArray[i];
+                inArray[i]=inArray[j];
+                inArray[j]=k;
+            }
         }
     }
 }
-FillMatrix(matrix, 1, 9);
 
-void PrintMatrix(int[,] matr)
+
+void PrintArray(int[,] inArray)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < inArray.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            Console.Write($"{matr[i, j]} ");
+            Write($"{inArray[i, j]} ");
         }
-        Console.WriteLine();
+        WriteLine();
     }
-    Console.WriteLine();
 }
-PrintMatrix(matrix);
-Console.WriteLine();
+
+void PrintData(int[] inArray)
+{
+    int el=inArray[0];
+    int count = 1;
+    for (int i = 1; i < inArray.Length; i++)
+    {
+        if(inArray[i]!=el)
+        {
+            WriteLine($"{el} встречается {count}");
+            el=inArray[i];
+            count=1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    WriteLine($"{el} встречается {count}");
+}
