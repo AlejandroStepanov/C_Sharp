9291d620d4cbf9fdb70b0cd83b5f60f95428dd04
«Alexandr» <nazgul.nn@gmail.com>
2022-12-19 15:49:19 +0300

    HW 9

---
+++ Homework/Ex064_FromN_to1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до M.
+//  Выполнить с помощью рекурсии.
+
+// M = 5; N = 3 -> "5, 4, 3"
+// M = 8; N = 4. -> "8, 7, 6, 5, 4"
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+Write("Введите M: ");
+int m=int.Parse(ReadLine());
+
+string MtoN(int start, int end)
+{
+    if (end == start)
+    {
+        return end.ToString();
+    }
+    return end + " " + MtoN(start, end-1);
+
+}
+
+Console.WriteLine(MtoN(m, n));
+
+
+// void a(int n)
+// {
+//     if (n >= 1)
+//     {
+//         a(n - 1); //Уменьшает до минимума и при закрытии каждой функции выводит на печать
+//         Console.Write(n);
+//     }
+// }
+// a(7);
+
+// void FooNazad(int n)
+// {
+//     Console.Write($"{n} ");
+//     if(n>=1)
+//     {
+//         FooNazad(n-1);
+//     }
+// }
+
+// FooNazad(6);
---
+++ Homework/Ex066_Sum_M--N/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите M: ");
+int m=int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n=int.Parse(Console.ReadLine());
+
+int SumNtoM(int start, int end)
+{
+    if (start == end)
+    {
+        return end;
+    }
+    return start + SumNtoM(start + 1, end);
+}
+
+Console.WriteLine($"Сумма чисел от {m} до {n} = {SumNtoM(m, n)}");
+
+// {
+
+//     if(rank==0)//1
+//     {
+//         return 1;//n
+//     }
+//     // rank --;
+//     return n * Sum(n,rank-1);
+
+// }
